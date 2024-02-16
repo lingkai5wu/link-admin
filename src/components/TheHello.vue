4d296f233c9c461efb2c11f7267f2b8e66af226e
lingkai5wu <lingkai5wu@outlook.com>
2024-02-17 00:37:00 +0800

    feat: Hello

---
+++ src/components/TheHello.vue
@@ -0,0 +1,11 @@
+<template>
+  <n-flex align="center" class="container" justify="center">
+    <n-h1>Hello, Link Admin!</n-h1>
+  </n-flex>
+</template>
+
+<style scoped>
+.container {
+  min-height: 100vh;
+}
+</style>
---
+++ src/views/layout/LayoutView.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup>
+import TheHello from '@/components/TheHello.vue'
+</script>
+
+<template>
+  <TheHello />
+</template>
