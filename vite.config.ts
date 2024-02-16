707eb809ff768b892ee36be7972e1d4465744f8a
lingkai5wu <lingkai5wu@outlook.com>
2024-02-17 00:32:08 +0800

    chore: 配置自动导入

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,26 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      imports: [
+        'vue'
+      ],
+      eslintrc: {
+        enabled: true
+      }
+    }),
+    Components({
+      resolvers: [NaiveUiResolver()]
+    })
   ],
   resolve: {
     alias: {
