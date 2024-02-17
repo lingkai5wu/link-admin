e856be61404827f9299830265c91a8d16e40604a
lingkai5wu <lingkai5wu@outlook.com>
2024-02-17 12:24:30 +0800

    refactor: 修改基础布局的文件结构

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LayoutView from '../views/layout/LayoutView.vue'
+import LayoutView from '../views/BaseLayout.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
