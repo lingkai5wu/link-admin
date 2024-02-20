5086391ac88932b5d3ab644719aaabb9bfe1c3e8
lingkai5wu <lingkai5wu@outlook.com>
2024-02-20 14:11:05 +0800

    fix: 登录页添加跳转注册按钮

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -3,8 +3,12 @@ import TheLoginForm from '@/components/auth/TheLoginForm.vue'
 </script>
 
 <template>
-  <h2>登录您的账号</h2>
+  <n-h2>登录您的账号</n-h2>
   <TheLoginForm />
+  <n-p depth="2"
+    >还没有账号？
+    <n-button text>注册</n-button>
+  </n-p>
 </template>
 
 <style scoped></style>
