8f12d69b16545f64638038b533148d1aea614d29
lingkai5wu <lingkai5wu@outlook.com>
2024-02-22 23:20:52 +0800

    feat: 设置 Naive UI 全局配置

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+import { darkTheme, dateZhCN, useOsTheme, zhCN } from 'naive-ui'
+import { computed } from 'vue'
+
+const osTheme = useOsTheme()
+const theme = computed(() => (osTheme.value === 'dark' ? darkTheme : null))
+</script>
+
 <template>
-  <router-view />
+  <n-config-provider :locale="zhCN" :date-locale="dateZhCN" :theme="theme">
+    <n-layout>
+      <router-view />
+    </n-layout>
+  </n-config-provider>
 </template>
