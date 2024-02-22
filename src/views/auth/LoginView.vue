dc19850549f894c55f37c2af3deb6b2d878e4162
lingkai5wu <lingkai5wu@outlook.com>
2024-02-23 00:54:11 +0800

    feat: 简化登录

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,14 +1,61 @@
 <script lang="ts" setup>
-import TheLoginForm from '@/components/auth/TheLoginForm.vue'
+import InputSmsCode from '@/components/auth/InputSmsCode.vue'
+import LoadingButton from '@/components/LoadingButton.vue'
+import { sleep } from '@/utils/common'
+import type { FormItemRule } from 'naive-ui'
+
+const formData = ref({
+  phone: '18888888888',
+  smsCode: '888888'
+})
+const formRules = {
+  phone: [
+    {
+      required: true,
+      validator(_: FormItemRule, value: string) {
+        if (!value) {
+          return new Error('请输入手机号')
+        }
+        if (!/^1\d{10}$/.test(value)) {
+          return new Error('请输入正确的手机号')
+        }
+        return true
+      },
+      trigger: 'blur'
+    }
+  ],
+  smsCode: [
+    {
+      required: true,
+      validator(_: FormItemRule, value: string) {
+        if (!value) {
+          return new Error('请输入验证码')
+        }
+        if (!/^\d{6}$/.test(value)) {
+          return new Error('请输入正确的验证码')
+        }
+      },
+      trigger: 'blur'
+    }
+  ]
+}
+
+async function handleLogin() {
+  window.$message.info('模拟登录')
+  await sleep(1000)
+  window.$message.success('登录成功')
+}
 </script>
 
 <template>
   <n-h2>登录您的账号</n-h2>
-  <TheLoginForm />
-  <n-p depth="2"
-    >还没有账号？
-    <n-button text>注册</n-button>
-  </n-p>
+  <n-form ref="formRef" :model="formData" :rules="formRules">
+    <n-form-item label="手机号" path="phone">
+      <n-input v-model:value="formData.phone" />
+    </n-form-item>
+    <n-form-item label="验证码" path="smsCode">
+      <InputSmsCode v-model:value="formData.smsCode" :func="() => sleep(1000)" :interval="10" />
+    </n-form-item>
+    <LoadingButton :func="handleLogin" style="width: 100%; margin-top: 4px;" type="primary">登录</LoadingButton>
+  </n-form>
 </template>
-
-<style scoped></style>
