133e4be1535feebbf4bb9fdfa1903136c91eafb7
lingkai5wu <lingkai5wu@outlook.com>
2024-02-23 14:45:15 +0800

    feat: 封装 request

---
+++ src/utils/request.ts
@@ -0,0 +1,44 @@
+import router from '@/router/index.js'
+import { useAuthStore } from '@/stores/auth.js'
+import type { Result } from '@/types/api'
+import type { AxiosResponse } from 'axios'
+import axios from 'axios'
+
+const baseURL = '/api'
+const instance = axios.create({ baseURL })
+
+instance.interceptors.request.use((config) => {
+  const authStore = useAuthStore()
+  if (authStore.token) {
+    config.headers[authStore.token.tokenName] = authStore.token.tokenValue
+  }
+  return config
+})
+
+instance.interceptors.response.use(
+  async (response: AxiosResponse<Result<any>, any>) => {
+    if (response.data.code === 200) {
+      return response.data.data
+    }
+
+    window.$message.error(response.data.msg)
+    window.$loadingBar.error()
+    if (response.data.code === 401) {
+      const authStore = useAuthStore()
+      authStore.token = null
+      await router.replace('/login')
+    }
+    return Promise.reject(response.data)
+  },
+  (err) => {
+    window.$dialog.error({
+      title: '服务器错误',
+      content: '请联系管理员，等待服务恢复',
+      positiveText: '好的'
+    })
+    window.$loadingBar.error()
+    return Promise.reject(err)
+  }
+)
+
+export default instance
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
+import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 import Components from 'unplugin-vue-components/vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -25,6 +24,15 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
     }
   }
 })
