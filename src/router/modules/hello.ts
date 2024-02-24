2c80df236340bf5331f498ba17fa84e5bd6f3a1e
lingkai5wu <lingkai5wu@outlook.com>
2024-02-24 11:57:40 +0800

    feat: 移除 Hello

--- src/router/modules/hello.ts
+++
@@ -1,6 +0,0 @@
-import type { RouteRecordRaw } from 'vue-router'
-
-export default {
-  path: 'hello',
-  component: () => import('@/views/HelloView.vue')
-} as RouteRecordRaw
--- src/views/HelloView.vue
+++
@@ -1,7 +0,0 @@
-<script lang="ts" setup>
-import TheHello from '@/components/TheHello.vue'
-</script>
-
-<template>
-  <TheHello />
-</template>
