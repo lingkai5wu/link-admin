e76e8e5f7a4f6333205d55080b7d82e17c61bd1d
lingkai5wu <lingkai5wu@outlook.com>
2024-02-27 15:12:24 +0800

    fix: 修复 main.ts 兼容性问题

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+async function init() {
+  const app = createApp(App)
+  const pinia = createPinia()
+  pinia.use(piniaPluginPersistedstate)
+  app.use(pinia)
+  initNaiveUIDiscreteApi()
+  await initRuntimeData()
+  app.use(router)
+  app.mount('#app')
+}
 
-const pinia = createPinia()
-pinia.use(piniaPluginPersistedstate)
-app.use(pinia)
-initNaiveUIDiscreteApi()
-await initRuntimeData()
-app.use(router)
-
-app.mount('#app')
+init().then()
