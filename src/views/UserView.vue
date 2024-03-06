a41da6b45a2bb0fe53ecc2eb165f93e14b67a97a
lingkai5wu <lingkai5wu@outlook.com>
2024-03-06 15:18:04 +0800

    feat: 简单用户管理

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,7 +1,29 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { listUserVOs } from '@/api/user'
+import type { UserVO } from '@/types/api/vo'
+import type { DataTableColumns } from 'naive-ui'
+
+const columns = ref<DataTableColumns<UserVO>>([
+  {
+    title: '手机号',
+    key: 'phone'
+  },
+  {
+    title: '性别',
+    key: 'sex'
+  },
+  {
+    title: '状态',
+    key: 'status'
+  },
+  {
+    title: '创建时间',
+    key: 'createTime'
+  }
+])
+</script>
 
 <template>
   <h1>用户管理</h1>
+  <DataTable :data-table-props="{ columns: columns }" :func="listUserVOs" />
 </template>
-
-<style scoped></style>
