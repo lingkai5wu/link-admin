1cea3cb4e23e970bdff015133d5e723034ae27e5
lingkai5wu <lingkai5wu@outlook.com>
2024-03-06 12:14:49 +0800

    feat: 简单菜单管理

--- src/views/MenuView.vue
+++ src/views/MenuView.vue
@@ -1,7 +1,32 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { listMenus } from '@/api/menu'
+import type { MenuOptionWithRouteMeta } from '@/types/menu'
+import { generateMenuOptions } from '@/utils/menu'
+import type { DataTableColumns } from 'naive-ui'
+
+const tableData = ref<MenuOptionWithRouteMeta[]>()
+listMenus().then((data) => {
+  tableData.value = generateMenuOptions(data)
+})
+const columns = ref<DataTableColumns<MenuOptionWithRouteMeta>>([
+  {
+    title: '标签',
+    key: 'label'
+  },
+  {
+    title: '类型',
+    key: 'type'
+  },
+  {
+    title: '路径',
+    key: 'path'
+  }
+])
+</script>
 
 <template>
   <h1>菜单管理</h1>
+  <n-data-table :columns="columns" :data="tableData" />
 </template>
 
 <style scoped></style>
