e37a667e22c242422d8da3d8f292bff34db4fbc9
lingkai5wu <lingkai5wu@outlook.com>
2024-03-07 15:15:43 +0800

    fix: 修复菜单不随路径变化

--- src/components/layout/TheMenu.vue
+++ src/components/layout/TheMenu.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import router from '@/router'
 import { useMenuStore } from '@/stores/menu.js'
 import type { MenuOptionWithEx } from '@/types/menu'
 import { OpenOutline } from '@vicons/ionicons5'
@@ -6,7 +7,14 @@ import { NIcon } from 'naive-ui'
 import type { VNodeChild } from 'vue'
 import { RouterLink } from 'vue-router'
 
+const route = router.currentRoute
 const menuStore = useMenuStore()
+const menuValue = computed(() => {
+  const currentMenuVO = menuStore.menuVOs?.find((menuVO) => menuVO.path === route.value.path)
+  if (currentMenuVO) {
+    return currentMenuVO.id
+  }
+})
 
 function renderMenuLabel(menuOption: MenuOptionWithEx): VNodeChild {
   switch (menuOption.type) {
@@ -32,10 +40,10 @@ function renderMenuExtra(menuOption: MenuOptionWithEx): VNodeChild {
 
 <template>
   <n-menu
-    :default-value="$route.name as string"
     :options="menuStore.menuOptions"
     :render-extra="renderMenuExtra"
     :render-label="renderMenuLabel"
+    :value="menuValue"
     key-field="id"
   />
 </template>
