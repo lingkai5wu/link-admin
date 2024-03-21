c54ab51f15c6be8ebd070437a56bb49a887bdb3f
lingkai5wu <lingkai5wu@outlook.com>
2024-03-22 02:20:08 +0800

    feat: 封装 DataTableTransfer

--- src/components/curd/UserRoleEdit.vue
+++ src/components/curd/UserRoleEdit.vue
@@ -1,51 +1,32 @@
 <script lang="ts" setup>
-import { listRoleVOs } from '@/api/role'
-import { listUserRoleVOs, updateUserRoleByBatch } from '@/api/user'
+import { listCurrentUserRoleVOs, listRoleVOs } from '@/api/role'
+import { updateUserRoleByBatch } from '@/api/user'
+import DataTableTransfer from '@/components/data-table/DataTableTransfer.vue'
 import type { UserVO } from '@/types/api/vo'
-import type { TransferOption } from 'naive-ui'
 
-const props = defineProps<{
+defineProps<{
   row: UserVO
 }>()
-const emits = defineEmits<{
-  actionSubmit: [boolean]
-  actionFuncExec: [boolean]
-}>()
 
-const options = ref<TransferOption[]>()
-listRoleVOs().then((data) => {
-  options.value = data.map((roleVO) => ({
+async function getOptionsFunc() {
+  const roleVOs = await listRoleVOs()
+  return roleVOs.map((roleVO) => ({
     label: roleVO.name,
     value: roleVO.id
   }))
-})
-const value = ref<number[]>()
-let origin: number[]
-listUserRoleVOs(props.row.id).then((data) => {
-  origin = data.map((roleVO) => roleVO.id)
-  value.value = [...origin]
-})
+}
 
-async function handleClick() {
-  const originSet = new Set(origin)
-  const valueSet = new Set(value.value)
-  let targetIdsToInsert = value.value?.filter((id) => !originSet.has(id))
-  let targetIdsToDelete = origin.filter((id) => !valueSet.has(id))
-  emits('actionFuncExec', true)
-  try {
-    await updateUserRoleByBatch(props.row.id, { targetIdsToInsert, targetIdsToDelete })
-  } finally {
-    emits('actionFuncExec', false)
-  }
-  emits('actionSubmit', true)
+async function getValueFunc() {
+  const roleVOs = await listCurrentUserRoleVOs()
+  return roleVOs.map((roleVO) => roleVO.id)
 }
 </script>
 
 <template>
-  <n-flex :size="24" vertical>
-    <n-transfer v-model:value="value" :options="options" />
-    <loading-button :func="handleClick" style="width: 100%" type="primary">提交</loading-button>
-  </n-flex>
+  <DataTableTransfer
+    :id="row.id"
+    :get-options-func="getOptionsFunc"
+    :get-value-func="getValueFunc"
+    :submit-func="updateUserRoleByBatch"
+  />
 </template>
-
-<style scoped></style>
---
+++ src/components/data-table/DataTableTransfer.vue
@@ -0,0 +1,75 @@
+<script lang="ts" setup>
+import type LoadingButton from '@/components/LoadingButton.vue'
+import type { BatchManyToManyDTO } from '@/types/api/query'
+import { type TransferOption } from 'naive-ui'
+
+const props = defineProps<{
+  id: number
+  getOptionsFunc: () => Promise<TransferOption[]>
+  getValueFunc: () => Promise<number[]>
+  submitFunc: (id: number, dto: BatchManyToManyDTO) => Promise<null>
+}>()
+const emits = defineEmits<{
+  actionSubmit: [boolean]
+  actionFuncExec: [boolean]
+}>()
+
+const options = ref<TransferOption[]>()
+let origin: number[]
+const value = ref<number[]>()
+const isLoading = ref(false)
+const loadingButtonRef = ref<InstanceType<typeof LoadingButton> | null>(null)
+
+onMounted(() => {
+  fetchOptionsAndValue()
+})
+
+async function fetchOptionsAndValue() {
+  isLoading.value = true
+  try {
+    const [optionsData, valueData] = await Promise.all([
+      props.getOptionsFunc(),
+      props.getValueFunc()
+    ])
+    options.value = optionsData
+    origin = valueData
+    value.value = [...valueData]
+  } finally {
+    isLoading.value = false
+  }
+}
+
+async function handleClick() {
+  const originSet = new Set(origin)
+  const valueSet = new Set(value.value)
+  let targetIdsToInsert = value.value?.filter((id) => !originSet.has(id))
+  let targetIdsToDelete = origin.filter((id) => !valueSet.has(id))
+  emits('actionFuncExec', true)
+  try {
+    await props.submitFunc(props.id, { targetIdsToInsert, targetIdsToDelete })
+  } finally {
+    emits('actionFuncExec', false)
+  }
+  emits('actionSubmit', true)
+}
+</script>
+
+<template>
+  <n-flex :size="24" vertical>
+    <n-spin :show="isLoading">
+      <n-transfer
+        v-model:value="value"
+        :disabled="loadingButtonRef?.isLoading"
+        :options="options"
+      />
+    </n-spin>
+    <loading-button
+      ref="loadingButtonRef"
+      :disabled="isLoading"
+      :func="handleClick"
+      style="width: 100%"
+      type="primary"
+      >提交
+    </loading-button>
+  </n-flex>
+</template>
