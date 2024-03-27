4f942d0d516f2df09facc391dcd28f9ba2d310cd
lingkai5wu <lingkai5wu@outlook.com>
2024-03-28 00:49:03 +0800

    feat: 首屏加载动画

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <script lang="ts" setup>
 import { theme } from '@/utils/naive-ui'
 import { dateZhCN, NThemeEditor, zhCN } from 'naive-ui'
+
+onBeforeMount(() => {
+  const element = document.getElementById('banter-loader')
+  document.body.removeChild(element!)
+})
 </script>
 
 <template>
