4c132d491b7ce0dc775820c6e5621eb9be7668b7
lingkai5wu <lingkai5wu@outlook.com>
2024-05-15 21:46:06 +0800

    fix: 移除 n-theme-editor

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import { theme } from '@/utils/naive-ui'
-import { dateZhCN, NThemeEditor, zhCN } from 'naive-ui'
+import { dateZhCN, zhCN } from 'naive-ui'
 
 onBeforeMount(() => {
   const element = document.getElementById('banter-loader')
@@ -10,8 +10,6 @@ onBeforeMount(() => {
 
 <template>
   <n-config-provider :date-locale="dateZhCN" :locale="zhCN" :theme="theme">
-    <n-theme-editor>
-      <router-view />
-    </n-theme-editor>
+    <router-view />
   </n-config-provider>
 </template>
