b1e151569f6e15e02661a8e059fc35ac35812459
lingkai5wu <lingkai5wu@outlook.com>
2024-05-15 21:46:20 +0800

    fix: 修改登录页样式

--- src/views/auth/AuthLayout.vue
+++ src/views/auth/AuthLayout.vue
@@ -1,9 +1,17 @@
 <template>
   <n-layout>
-    <n-flex align="center" class="container" justify="center">
+    <n-flex align="center" class="container" justify="center" vertical>
+      <n-h1>社区养宠服务平台</n-h1>
+      <!-- TODO 调整标题 -->
       <n-card class="main">
         <router-view />
       </n-card>
+      <n-p
+        >登录即代表您已阅读并同意我们的
+        <n-text type="info">服务协议</n-text>
+        和
+        <n-text type="info">隐私政策</n-text>
+      </n-p>
     </n-flex>
   </n-layout>
 </template>
